9c49c407f0e0816df9a5422c9b3ec910ddd32e6e
gilangaryap <gilang2001.ap@gmail.com>
2024-10-08 17:58:41 +0700

    Improve input

--- src/components/Input/IceHotInput.tsx
+++ src/components/Input/IceHotInput.tsx
@@ -11,7 +11,7 @@ export const IceHotInput = ({
     <div className="flex space-x-4">
       <button
         onClick={() => onOptionChange(1)}
-        className={`text-lg font-bold py-2 w-full border border-solid text-lightgray hover:border-primary active:bg-darkgray focus:border-primary focus:text-black ${
+        className={`text-lg py-2 w-full border border-solid text-lightgray hover:border-primary active:bg-darkgray focus:border-primary focus:text-black ${
           selectedOption === 1 ? "bg-transparent border-primary text-black" : "bg-transparent border-[#E8E8E8]"
         }`}>
         Hot
